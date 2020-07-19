bbaf637c12897e5d8edf9da00e8b3f51bba2b207
Evandro Veloso Gomes <evandro.gomes@e-smart.com.br>
2020-07-19 17:29:57 -0300

    Commited .env

--- tests/Controller/IndexControllerTest.php
+++ tests/Controller/IndexControllerTest.php
@@ -6,6 +6,11 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class IndexControllerTest
+ *
+ * @package App\Tests\Controller
+ */
 class IndexControllerTest extends WebTestCase
 {
     public function testIndex()
@@ -26,6 +31,22 @@ class IndexControllerTest extends WebTestCase
                 'companySymbol' => 'AAIT',
                 'startDate'     => '2020-07-01',
                 'endDate'       => '2020-07-02',
+                'email'         => 'a@a.com',
+            ]
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndexPostFail()
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/',
+            [
+                'companySymbol' => 'AAIT',
+                'startDate'     => '2020-07-02',
+                'endDate'       => '2020-07-01',
                 'email'         => 'a@a.com',
             ]
         );
